052fe25d93c9aa1cc5a80356265efee0d3a974f5
尊明 藤木 <fujikit@jae.co.jp>
2020-09-08 17:35:13 +0900

    不要なコメント削除

--- src/main/java/com/takaaki/urcap/dialogscript2/impl/xmlrpc/DialogScriptXmlRpcServer.java
+++ src/main/java/com/takaaki/urcap/dialogscript2/impl/xmlrpc/DialogScriptXmlRpcServer.java
@@ -12,19 +12,15 @@ public class DialogScriptXmlRpcServer extends XmlRpcServer {
 
     public DialogScriptXmlRpcServer(DialogScriptInstallationNodeContribution contribution, int port) throws Exception {
 
-        // ハンドラクラスの初期化-------
         DialogScriptHandler.initialize(contribution);
 
-        // XMLRPCサーバ初期化
         webServer = new WebServer(port);
         XmlRpcServer xmlRpcServer = webServer.getXmlRpcServer();
 
-        // ハンドラ登録
         PropertyHandlerMapping phm = new PropertyHandlerMapping();
         phm.addHandler("dialog", DialogScriptHandler.class);
         xmlRpcServer.setHandlerMapping(phm);
 
-        // XMLRPCサーバ準備
         XmlRpcServerConfigImpl config = (XmlRpcServerConfigImpl) xmlRpcServer.getConfig();
         config.setEnabledForExtensions(true);
         config.setContentLengthOptional(false);
